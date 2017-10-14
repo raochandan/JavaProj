f41cfe8d8bbb1bb35868e9c5ced6a8a05b29b2ae
chandan.singhgit config --global user.name chandan.singh <chandan_9818in@yahoo.com>
2017-10-14 17:40:26 +0530

    addedfiles

---
+++ src/com/foo/HelloWorldtesting.java
@@ -0,0 +1,12 @@
+package com.foo;
+
+import java.math.BigDecimal;
+public class HelloWorldtesting {//Quick fix CTRL 1
+
+		private static Integer integer;
+		public static void main(String[] args) {
+				BigDecimal bigDecimal = new BigDecimal(5.0);//ctrl + space
+
+	}
+//
+}
