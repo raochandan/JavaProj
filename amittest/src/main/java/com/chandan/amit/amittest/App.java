a67be6504261dd681790076bcc652a982ce491c4
chandan.singhgit config --global user.name chandan.singh <chandan_9818in@yahoo.com>
2017-10-14 17:57:18 +0530

    update

---
+++ amittest/src/main/java/com/chandan/amit/amittest/App.java
@@ -0,0 +1,13 @@
+package com.chandan.amit.amittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
